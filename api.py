772d41065adf953096942f83c04ed24cae5e5306
druhithad <153919974+druhithad@users.noreply.github.com>
2026-09-04 21:51:29 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,11 +4,11 @@ import tempfile
 import joblib
 import pandas as pd
 
-from src.speech_detector import detect_speech
-from src.speech_to_text import speech_to_text
 from src.predict_audio import extract_features, load_feature_columns
 
+
 app = FastAPI(title="Sound2See API")
+
 
 MODEL_PATH = "models/sound2see_final.joblib"
 
@@ -22,7 +22,9 @@ if classifier is not None and not hasattr(classifier, "multi_class"):
 
 @app.get("/")
 def home():
-    return {"message": "Sound2See API is running"}
+    return {
+        "message": "Sound2See API is running"
+    }
 
 
 @app.post("/detect")
@@ -39,18 +41,6 @@ async def detect(file: UploadFile = File(...)):
         audio_path = temp.name
 
     try:
-
-        is_speech = detect_speech(audio_path)
-
-        if is_speech:
-
-            text = speech_to_text(audio_path)
-
-            return {
-                "type": "speech",
-                "text": text,
-                "sound": "Human Speech"
-            }
 
         features = extract_features(audio_path)
 
